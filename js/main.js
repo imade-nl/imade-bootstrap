a938535e94f44760b5bdfd30055e719eba36b91b
imade.nl <imade.nl>
2014-03-26 14:34:32 +0100

    little tweaks

--- js/main.js
+++ js/main.js
@@ -26,7 +26,9 @@ $('a[rel="external"], a.blank').click( function(e) {
 
 /*
 
-$("#content").highlight( $('#input_search').val() );
+if( $('#input_search').length ){
+	 $("#content").highlight( $('#input_search').val() );
+}
 
 $('a[href^="http://"]').attr("target", "_blank");
 
